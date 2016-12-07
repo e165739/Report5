6e94ab49a62bb0a488db34c335447a01d486dac2
Asayama Ibuki <e165739@ie.u-ryukyu.ac.jp>
2016-12-07 19:26:38 +0900

    Report5,Step1終了

--- src/jp/ac/uryukyu/ie/e165739/Step1.java
+++ src/jp/ac/uryukyu/ie/e165739/Step1.java
@@ -4,4 +4,8 @@ package jp.ac.uryukyu.ie.e165739;
  * Created by asayamaibuki on 2016/12/05.
  */
 public class Step1 {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
 }
